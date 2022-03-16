6ece79678240e698fbf0633aab220fd100d36a7b
jasminbecic <becicjasmin1@gmail.com>
2022-03-16 14:29:35 -0500

    created whitelist in AuthenticationFilter class and updated preHandle method for whitelist

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
@@ -10,8 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
 
     @Autowired
     UserRepository userRepository;
@@ -19,10 +23,25 @@ public class AuthenticationFilter extends HandlerInterceptorAdapter {
     @Autowired
     AuthenticationController authenticationController;
 
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
                              Object handler) throws IOException {
+
+        // Don't require sign-in for whitelisted pages
+        if (isWhitelisted(request.getRequestURI())) {
+            // returning true indicates that the request may proceed
+            return true;
+        }
 
         HttpSession session = request.getSession();
         User user = authenticationController.getUserFromSession(session);
